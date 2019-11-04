29a4818ce6cc7eee7e7ddedbc0738df1510b5749
everton-canez <everton-canez@procergs.rs.gov.br>
2019-11-04 16:51:55 -0300

    Tests with conditionals

--- conditionals/messages.js
+++ conditionals/messages.js
@@ -3,19 +3,6 @@
  * in javascript without IFs nor Switches
  */
 
-const getPrimitive1 = (type) => ({
-    'string': 'A sequence os characteres.',
-    'boolean': 'Logical data type that can have the values true or false.'
-} [type] || 'The especified value is not a string or boolean.')
-
-const test1 = getPrimitive1('boolean')
-console.log(test1)
-
-const test2 = getPrimitive1('string')
-console.log(test2)
-
-console.log('==============================')
-
 const getMessage = (err) => {
     return ({
         'NotFoundError': {statusCode: 404,message: "None record found!"},
